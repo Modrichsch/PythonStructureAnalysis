a04119444b65e62bace926b78ad5a36c43e3f5e5
Modrichsch <famtel1810@gmail.com>
2025-10-12 13:20:38 +0200

    Correct angles

--- Nodes_analysis.py
+++ Nodes_analysis.py
@@ -1,11 +1,17 @@
 import math
 
-nodes = [(0,0.5,0), #3
-         (0,0.5,0.5), #3'
-         (0.3,0.5,0), #9
-         (0.3,0.5,0.5), #9'
-         (0.35,0.85,0.25)] #10
+nodes = {
+    0: (0, 0, 0),
+    1: (0.5, 0.5, 0),
+    2: (0.5, 0, 0),
+    3: (0,0.5,0), 
+    4: (0.18, 0.8, 0.25),
+    5: (1.1, 0.6, 0.25),
+    6: (0.7, 0.85, 0.25),
+    9: (0.3,0.5,0), 
+    10: (0.35,0.85,0.25)} 
 
+#for comparing to our design before the changes
 force_dict = {
     -1: -3,
     6: 1.716,
@@ -18,58 +24,50 @@ force_dict = {
     13: 1.825,
     14: 3.2,
     15: -2.507,
+    17: -1.152,
+    18: 4.469,
     19: 5.143,
     20: -1.366,
 }
 calc_forces = [0, 0, 0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0, 0,0, 0,0, 0,0, 0,0, -3] #1-20 and the last one "-1" is force F
-#angles are between 0 and 0.5pi, where 0 is horizontal and the pi is left out for clear comparison.
-# cos(a) will be the horizontal component and sin(a) the vertical
-angle_dict = {
-    -1: [0.5, 0, 0.5],
-    6: [0.5, 0.0, 0.5],
-    7: [0.32797913037736937, 0.17202086962263075, 0.5],
-    8: [0.6211189415908434, 0.12111894159084333, 0.5],
-    9: [0.0, 0.5, 0.5],
-    10: [0.0, 0.5, 0.5],
-    11: [0.285, 0.435, 0.229],
-    12: [0.594897731703665, 0.23749504804274926, 0.2903876877151751],
-    13: [0.4631625679551186, 0.2003931591710909, 0.3040867239846964],
-    14: [0.12566591637800234, 0.3743340836219976, 0.5],
-    15: [0.6068103096969111, 0.22107814162091644, 0.3150767403019983],
-    17: [0.13427118575450062, 0.4514102096524711, 0.3758747875120568],
-    18: [0.1778076844893528, 0.3221923155106473, 0.5],
-    19: [0.0, 0.5, 0.5],
-    20: [0.5, 0.5, 0.0]
-}
 
-angle : float = 0
-position = (0.18,0.8,0.25)
-deltaX = 0.0  #fill in the desired value here
-deltaY = -0.05  #fill in the desired value here
-new_pos = []
-
-new_pos.append(position[0] + deltaX)
-new_pos.append(position[1] )#+ deltaY)
-new_pos.append(position[2])
+angles : tuple = [0]*22 #in sin
 
 
-def angler(pos2):
-    dx = new_pos[0]-pos2[0]
-    dy = new_pos[1]-pos2[1]
-    return math.atan(dy/dx)
+def angler(n1, n2, m):
+    dx = nodes[n2][0] - nodes[n1][0]
+    dy = nodes[n2][1] - nodes[n1][1]
+    dz = nodes[n2][2] - nodes[n1][2]
+    x = math.sqrt(dx**2 + dz**2)
+    angles[m] = math.atan(dy/(x+0.00000000001)) #creates the angles. The 0.0...1 is to prevent division by 0.
 
-def forceX(mF, new):
+angler(0, 3, 6)
+angler(0, 9, 7)
+angler(2, 9, 8)
+angler(3, 9, 9)
+angler(9, 1, 10)
+angler(3, 4, 11)
+angler(9, 4, 12)
+angler(9, 10, 13)
+angler(4, 10, 14)
+angler(1, 10, 15)
+angler(1, 6, 16)
+angler(1, 5, 17)
+angler(5, 6, 18)
+angler(10, 6, 19)
+angles.append(0)
+angles[-1] = 0.5*math.pi
+
+def forceX(mF):
     Fx = 0
     for i in range(len(mF)):
-        Fx += calc_forces[mF[i]]*math.cos(angle_dict[mF[i]][0]*math.pi)
-    if Fx < 0.01:
-        Fx = 0
+        Fx += calc_forces[mF[i]]*math.cos(angles[mF[i]])
     return Fx
 
-def forceY(mF, new):
+def forceY(mF):
     Fy = 0
     for i in range(len(mF)):
-        Fy += calc_forces[mF[i]]*math.cos(angle_dict[mF[i]][1]*math.pi)
+        Fy += calc_forces[mF[i]]*math.sin(angles[mF[i]])
     return Fy
 
 def solve_joint(mForces, newForces, kg):
@@ -77,15 +75,15 @@ def solve_joint(mForces, newForces, kg):
         print("too many unknowns!")
         return
     #sum of forces in X
-    Fx = forceX(mForces, newForces)
+    Fx = forceX(mForces)
     #sum of forces in Y
-    Fy = forceY(mForces, newForces)
+    Fy = forceY(mForces)
 
     #just to make the code "cleaner" will be replaced by a function that computes the angle instead of getting them from the list
-    angleX1 = math.cos(angle_dict[newForces[0]][0]*math.pi)
-    angleY1 = math.cos(angle_dict[newForces[0]][1]*math.pi)
-    angleX2 = math.cos(angle_dict[newForces[1]][0]*math.pi)
-    angleY2 = math.cos(angle_dict[newForces[1]][1]*math.pi)
+    angleX1 = math.cos(angles[newForces[0]])
+    angleY1 = math.cos(angles[newForces[1]])
+    angleX2 = math.sin(angles[newForces[0]])
+    angleY2 = math.sin(angles[newForces[1]])
 
     if not newForces[1]: #only one unknown, easy case
         T1 = -Fx/(angleX1*kg[0])
@@ -116,15 +114,13 @@ def solve_joint(mForces, newForces, kg):
     return T1, T2
 
 #sum of forces in joint 5
-print(solve_joint([-1], [17, 18], [2, 1]))
+solve_joint([-1], [17, 18], [2, 1])
+solve_joint([18], [16, 19], [2, 1])
+solve_joint([17, 20], [16, 19], [2, 1])
+for i in range(20):
+    print(i, calc_forces[i])
+#material use function:
 
-#material use:
-for y in range(5):
-    material = 0 #m
-    for i in range(5):
-        material += math.dist(new_pos, nodes[i])
-    print(material, "meters of material at y =", new_pos[1])
-    new_pos[1] += deltaY
 
 #angle calculation automation
 for i in range(1):
